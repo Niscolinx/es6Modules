7c3d93dd704e177df2b55ae8debccf9c1cdaa5cf
Niscolinx <munisco12@gmail.com>
2020-03-23 21:00:07 +0100

    updated the user-profile

--- public/js/components/user-profile.js
+++ public/js/components/user-profile.js
@@ -3,7 +3,6 @@ class userProfile {
     loadUserProfile(){
         console.log('loaded from the userProfile')
         console.log('loaded from the userProfile')
-        console.log('loaded from the userProfile')
     }
 }
 
